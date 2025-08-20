afec4a7cbf85befbbda435b6477c173240a9ece4
yashsisodiya123 <sisodiyay35@gmail.com>
2025-08-20 18:07:08 +0530

    Update largest bw two numbers.c

--- largest bw two numbers.c
+++ largest bw two numbers.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-void main(){
+int main(){
 int a,b;
 printf("enter the two numbers;\n");
 scanf("%d %d", &a,&b);
@@ -8,3 +8,4 @@ scanf("%d %d", &a,&b);
     else
     printf("%d is largest", b);
 }
+
