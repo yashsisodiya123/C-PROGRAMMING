54a45b85bec383ee103a147bc0f719e7f9befcd0
yashsisodiya123 <sisodiyay35@gmail.com>
2025-08-22 14:17:18 +0530

    Add files via upload

---
+++ if else average marks.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+float sub1, sub2, sub3;
+
+printf("sub1: ");
+scanf("%f", &sub1);
+
+printf("sub2: ");
+scanf("%f", &sub2);
+
+printf("sub3: ");
+scanf("%f", &sub3);
+
+    if(((sub1+sub2+sub3)/3)>=70)
+        printf("distinction");
+
+    else {
+            if(((sub1+sub2+sub3)/3)>=60)
+            printf("first class");
+
+                else {
+                      if(((sub1+sub2+sub3)/3)>=50)
+                      printf("second class");
+
+                        else{
+                                if(((sub1+sub2+sub3)/3)>=35)
+                                printf("third class");
+
+                                    else{
+                                        if(sub1<35||sub2<35||sub3<35)
+                                        printf("student fails");
+                                    }}}}}
---
+++ if else divisible by 7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int number, divisor;
+
+printf("number: ");
+scanf("%d", &number);
+
+    if(number%7==0)
+        printf("%d is divisible by 7.", number);
+    else
+        printf("%d is not divisible by 7.", number);
+
+        }
