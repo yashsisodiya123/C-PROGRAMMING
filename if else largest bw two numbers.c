8ff6a1196802da775377ab784c28960cc27c6897
yashsisodiya123 <sisodiyay35@gmail.com>
2025-09-27 18:22:33 +0530

    Update if else largest bw two numbers.c

--- if else largest bw two numbers.c
+++ if else largest bw two numbers.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main(){
 int a,b;
-printf("enter the two numbers;\n");
+printf("enter the two numbers:\n");
 scanf("%d %d", &a,&b);
     if(a>b)
         printf("%d is largest", a);
     else
     printf("%d is largest", b);
 }
+
 
